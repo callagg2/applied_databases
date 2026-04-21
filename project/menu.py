32751c61d42023480d11d5b8560ca7f7da35d5a3
Gerry Callaghan <gerrycallaghan2@gmail.com>
2026-04-21 22:31:35 +0100

    improved the part finding attendees

--- project/menu.py
+++ project/menu.py
@@ -1,5 +1,5 @@
 import menuDB
-from datetime import datetime, date # https://stackoverflow.com/questions/9504356/convert-string-into-date-type-on-python
+from datetime import datetime, date  # https://stackoverflow.com/questions/9504356/convert-string-into-date-type-on-python
 
 # Main function
 
@@ -7,60 +7,68 @@ def main():
 	# Initialise array
 	array = []
 	display_menu()
-	
+
 	while True:
 		choice = input("Choice: ")
-		
-		if (choice == "1"):
+
+		if choice == "1":
 			speaker_name = input("\nEnter speaker name: ")
-			print(f"Sessions Details For: {speaker_name}")
-			print(f"---------------------")
-			if "dr" in speaker_name:
-				#print(f"Sessions Details For: {speaker_name}")
-				#print(f"---------------------\n")
-				print(f"{speaker_name} | Session Title | Room")
-				display_menu()
+			possible_speakers = menuDB.find_speaker(speaker_name)
+			if possible_speakers != ():
+				print(f"Sessions Details For: {speaker_name}")
+				print(f"----------------------------")
+				for speaker in possible_speakers:
+					print(f"{speaker["speakerName"]} | {speaker["sessionTitle"]} |  {speaker["roomID"]}")
 			else:
 				print(f"No speakers found of that name")
-				display_menu()
             
-            #print("in 1")
-            #array = fill_array()
-            
-	
-		elif (choice == "2"):
+			display_menu()
+
+
+		elif choice == "2":
 			while True:
 				company_invalid = True
 				no_attendees = False
 				company_id = int(input("\nEnter Company ID number: "))
-				if (company_id > 0) and (company_invalid) is False:
+				valid_company_id = menuDB.view_attendees(company_id)
+                
+				if company_id > 0 and valid_company_id != ():
 					print(f"CloudSprint Attendees")
 					print(f"attendee1 | attendee 1 DOB | Session Title | Speaker Name | Date | Room")
 					print(f"attendee1 | attendee 2 DOB | Session Title | Speaker Name | Date | Room")
 					print(f"attendee1 | attendee 3 DOB | Session Title | Speaker Name | Date | Room")
 					print(f"attendee1 | attendee 4 DOB | Session Title | Speaker Name | Date | Room")
-				    #display_menu()
-				elif ((company_id > 0) and (company_invalid) is True and (no_attendees)) is False:
+					# display_menu()
+					break
+				elif company_id > 0 and valid_company_id () and not no_attendees:
 					print(f"Company with ID {company_id} doesn't exist")
-				elif (((company_id > 0) and (company_invalid) is True) and (no_attendees)) is True:
+					break
+				elif company_id > 0 and valid_company_id != () and no_attendees:
 					print(f"No attendees found for Company")
-		
-		
-					
-				    #print(f"No speakers found of that name")
-	                #display_menu()
-            
-            #print("in 2")
-			#print(array)
-			#display_menu()
-		elif (choice == "3"):
+					break
+				else:
+					print("Invalid input, try again.")
+
+			# print("in 2")
+			# print(array)
+			# display_menu()
+
+		elif choice == "3":
 			attendee_id = int(input("Enter Attendee's ID number: "))
-			attendee_name = (input("Enter Attendee's name: "))
-			attendee_DOB = (input("Enter Attendee's date of birth (yyyy-mm-dd): "))
+			attendee_name = input("Enter Attendee's name: ")
+			attendee_DOB = input("Enter Attendee's date of birth (yyyy-mm-dd): ")
 			attendee_DOB = datetime.strptime(attendee_DOB, '%Y-%m-%d').date()
-			print(f"{(attendee_DOB)} is of type {type(attendee_DOB)}")
-			attendee_gender = (input("Enter Attendee's gender: "))
+			print(f"{attendee_DOB} is of type {type(attendee_DOB)}")
+			attendee_gender = input("Enter Attendee's gender: ")
+			if attendee_gender not in ["Male", "Female"]:
+				print(f"*** ERROR *** Gender must be Male/Female")
+			
 			attendee_company_ID = int(input("Enter Attendee's Company ID number: "))
+			# Assuming company_invalid is a boolean; fix logic
+			#if company_invalid:  # Placeholder; adjust based on actual validation
+			#	print(f"*** ERROR *** Company Id: {attendee_company_ID} does not exist")
+			#	continue
+
 			print(f"\nAdd New Attendee")
 			print(f"------------------")
 			print(f"Attendee ID: {attendee_id}")
@@ -69,49 +77,55 @@ def main():
 			print(f"Attendee Gender: {attendee_gender}")
 			print(f"Company ID: {attendee_company_ID}")
 			print(f"\nAttendee successfully added")
-			attendees = menuDB.populate_data(attendee_id, attendee_name, attendee_DOB, attendee_gender, attendee_company_ID)
-			print(attendees)
-			#find_gt_in_array(array)
+			new_attendee = menuDB.populate_data(attendee_id, attendee_name, attendee_DOB, attendee_gender, attendee_company_ID)
+			attendees = menuDB.show_data()
+
+			# find_gt_in_array(array)
 			display_menu()
-		elif (choice == "x"):
-			break;
+
+		elif choice == "x":
+			break
 		else:
 			print("in else")
-			#display_menu()
-			
-'''	
-def fill_array():
+			display_menu()
+
+
+def view_connected_attendees():
 	array = []
 	num_enter = int(input("Enter number: "))
 	while num_enter != -1:
 		array.append(num_enter)
 		num_enter = int(input("Enter number: "))
 	return array
-	
 
+
+'''
 def find_gt_in_array(array):
 	number_entered = int(input("Enter number: "))
 	new_array = []
 	for num in array:
 		if num > number_entered:
 			new_array.append(num)
-	
-	print (new_array)
+
+	print(new_array)
 
 '''
+
+
 def display_menu():
-    print(f"\n\n\n Conference Management")
-    print(f"---------------------\n")
-    print("MENU")
-    print("=" * 4)
-    print("1 - View Speakers & Sessions")
-    print("2 - View Attendees by Company")
-    print("3 - Add New Attendee")
-    print("4 - View Connected Attendees")
-    print("5 - Add Attendee Connection")
-    print("6 - View Rooms")
-    print("x - Exit appliation")
+	print(f"\n\n\n Conference Management")
+	print(f"---------------------\n")
+	print("MENU")
+	print("=" * 4)
+	print("1 - View Speakers & Sessions")
+	print("2 - View Attendees by Company")
+	print("3 - Add New Attendee")
+	print("4 - View Connected Attendees")
+	print("5 - Add Attendee Connection")
+	print("6 - View Rooms")
+	print("x - Exit appliation\n")
+
 
 if __name__ == "__main__":
-	# execute only if run as a script 
+	# execute only if run as a script
 	main()
--- project/menuDB.py
+++ project/menuDB.py
@@ -1,4 +1,5 @@
 import pymysql
+from requests import session
 
 conn = None
 
@@ -6,31 +7,86 @@ conn = None
 
 def connect():
     global conn
-    conn = pymysql.connect(user="root",password="root",host="localhost",db="appdbproj",port=3306,cursorclass=pymysql.cursors.DictCursor)
+    conn = pymysql.connect(host="localhost",user="root",password="root",db="appdbproj",port=3306,cursorclass=pymysql.cursors.DictCursor)
+
+
+def find_speaker(namestr):
+    if (not conn):
+        connect()
+    
+    query1 = "SELECT speakerName, sessionTitle, roomID FROM session where locate(%s, speakerName) != 0 Order by speakerName"
+    values = ({namestr})
+    
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute(query1,values)
+        speakers = cursor.fetchall()
+        return speakers       
+        cursor.close()  # closes the cursor
+        #conn.close() # closes the connection
+        
+
+
+def view_attendees(company_id):
+    if (not conn):
+        connect()
+    
+    query2 = "SELECT companyID from company WHERE companyID = %s;"
+    values = company_id
+    #print(query2)
+    
+    with conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute(query2, values)
+            valid_company = cursor.fetchone()
+            return valid_company
+            #for attendee in attendees:
+                #print(attendee["attendeeID"], attendee["attendeeName"], attendee["attendeeDOB"], attendee["attendeeGender"], attendee["attendeeCompanyID"])    
+        except pymysql.err. InternalError as e:
+            print("Company ID: {company_id} doesn't exist")
+    
+    #cursor.close()  # closes the cursor
+    #conn.close() # closes the connection
+
+
+
+
+
+
+
+
+
+
 
 def populate_data(attendee_id, attendee_name, attendee_DOB, attendee_gender, attendee_company_ID):
     if (not conn):
         connect();
-    #attendee_id = 121
-    #attendee_name = "Joe Kelly"
-    attendee_DOB = "1970-02-18"
-    attendee_gender = "Male"
-    #attendee_company_ID = 2
-    #Executing a query
-    #query = "INSERT into attendee (attendeeID, attendeeName,  attendeeCompanyID) VALUES (%s, %s, %s)"
-    #values = ({attendee_id}, "{attendee_name}", {attendee_company_ID})
-    query = "INSERT into attendee (attendeeID, attendeeName, attendeeDOB, attendeeGender, attendeeCompanyID) VALUES (%s, %s, %s, %s, %s)"
-    values = ({attendee_id}, "{attendee_name}", "{attendee_DOB}", "{attendee_gender}", {attendee_company_ID})
-        
-    #query = "SELECT * from attendee"
-
+    query3 = "INSERT into attendee (attendeeID, attendeeName, attendeeDOB, attendeeGender, attendeeCompanyID) VALUES (%s, %s, %s, %s, %s)"
+    #query3 = "INSERT into attendee (attendeeID, attendeeName, attendeeDOB, attendeeGender, attendeeCompanyID) VALUES (attendee_id, {attendee_name}, {attendee_DOB}, {attendee_gender}, attendee_company_ID)"
+    values = (({attendee_id}), ({attendee_name}), ({attendee_DOB}), ({attendee_DOB}), ({attendee_company_ID}))
+    #print(query)
+    
     with conn:
-        cursor = conn.cursor()
-        #cursor.execute(query)
-        cursor.execute(query, values)
-        attendees = cursor.fetchall()
-        for attendee in attendees:
-            print(attendee["attendeeID"], attendee["attendeeName"], attendee["attendeeCompanyID"])
-            #print(attendee["attendeeID"], attendee["attendeeName"], attendee["attendeeDOB"], attendee["attendeeGender"], attendee["attendeeCompanyID"])
-        #print (attendees)
+        try:
+            cursor = conn.cursor()
+            cursor.execute(query3, (values))
+            #cursor.execute(query3, (121,"Joe Kelly","1970-02-18","Male",2))
+            conn.commit()
+            print("Insert Successful")
+        except pymysql.err. InternalError as e:
+            print("Internal Error")
+        except pymysql.err.IntegrityError:
+            print("Attendee ID: {attendee_id} already exists")
+        except Exception as e:
+            print("error",e)
+    
+    #cursor.close()  # closes the cursor
+    #conn.close() # closes the connection
+    
+        
+
+        
+       
+ 
     
